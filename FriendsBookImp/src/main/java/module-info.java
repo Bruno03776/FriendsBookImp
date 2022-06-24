d47a0481bd7844502a4789013d2866b0ea2f49f3
Bruno03776 <59750452+Bruno03776@users.noreply.github.com>
2022-06-23 17:52:46 -0700

    Delete FriendsBookImp directory

--- FriendsBookImp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.friendsbookimp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.friendsbookimp to javafx.fxml;
-    exports com.example.friendsbookimp;
-}
